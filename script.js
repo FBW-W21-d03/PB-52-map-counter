208c194eba44dde8d853e79f21586d7ec1e19447
ShiinyFlakes <mago_elm@hotmail.com>
2022-01-11 14:08:51 +0100

    Lösung

---
+++ script.js
@@ -0,0 +1,10 @@
+const zahlen = [80, 12, 57, 403, 3, 17, 88, 102, 31, 0, 1, 90, 250, 83]
+let counter = 0;
+
+zahlen.map(
+    (zahlen)=>{
+        counter +=zahlen;
+    }
+);
+
+console.log(counter);
