4c1e5c5bf9c3617aafef31e1867e52511fc2db48
OrhanKadirov <orhan.kadirov.gs@gmail.com>
2022-01-11 14:18:16 +0100

    Orhan's lösung

--- javascript-map-counter.js
+++ javascript-map-counter.js
@@ -1,7 +1,7 @@
 const ersteArray = [80, 12, 57, 403, 3, 17, 88, 102, 31, 0, 1, 90, 250, 83];
 
 let sum = 0;
-ersteArray.forEach((betrag) => {
+ersteArray.map((betrag) => {
     sum += betrag;
 });
 
