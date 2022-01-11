55a9f091f584f590d1eea7bcc38cd64034a186f5
Hayat Alkheder <hayat.alkheder@gmail.com>
2022-01-11 13:27:36 +0100

    map

---
+++ index.js
@@ -0,0 +1,16 @@
+// 1 )
+const zahlen = [
+    
+80, 12, 57, 403, 3, 17, 88, 102, 31, 0, 1, 90, 250, 83
+
+];
+console.log(zahlen);
+
+// 2 )
+let total = 0;
+
+zahlen.map(function (namber) {
+    total += namber;
+});
+
+console.log("Die Summe aller Zahlen ist :", total);
