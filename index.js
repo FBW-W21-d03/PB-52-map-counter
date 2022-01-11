b82888f7f8a1902ff3ab5b8725e83e1d6079e190
Sharfuddin Ahmed <Sharfuddinahmed07@gmail.com>
2022-01-11 14:16:25 +0100

    meine Lösung

---
+++ index.js
@@ -0,0 +1,13 @@
+const meineZahl = [80, 12, 57, 403, 3, 17, 88, 102, 31, 0, 1, 90, 250, 83];
+
+
+let summe = 0;
+
+meineZahl.map(function(meineZahl) {
+
+  summe += meineZahl;
+
+
+});
+
+console.log("Ergebnis ist:", summe); 
