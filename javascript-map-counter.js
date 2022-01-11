2e4b03305237b15becb0a70d9a222476964adf8d
OrhanKadirov <orhan.kadirov.gs@gmail.com>
2022-01-11 14:16:32 +0100

    Orhan's lösung

---
+++ javascript-map-counter.js
@@ -0,0 +1,9 @@
+const ersteArray = [80, 12, 57, 403, 3, 17, 88, 102, 31, 0, 1, 90, 250, 83];
+
+let sum = 0;
+ersteArray.forEach((betrag) => {
+    sum += betrag;
+});
+
+console.log("Array zahlen \n",ersteArray,);
+console.log("Summe die Array zahlen = ", sum);
